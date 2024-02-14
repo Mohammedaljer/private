7b5cba767f205cdb54c0b447e9713c42ef10fa7d
Mohammedaljer <134400465+Mohammedaljer@users.noreply.github.com>
2024-02-14 17:32:55 +0100

    Delete r2EncTest directory

--- r2EncTest/src/main/java/com/example/r2EncTest/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.r2EncTest;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface PersonRepository extends ReactiveCrudRepository<Person, Long> {
-	
-}
