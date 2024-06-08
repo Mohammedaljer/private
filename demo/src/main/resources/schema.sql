8f5aa80699df821d97c21bf50514ff5b3564e764
Mohammedaljer <134400465+Mohammedaljer@users.noreply.github.com>
2024-06-08 21:49:45 +0200

    Delete demo directory

--- demo/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-customer (id int AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(255) NOT NULL,
-last_name VARCHAR(255) NOT NULL,
-data VARCHAR(255) NOT NULL,
-CONSTRAINT unique_customer_name UNIQUE (first_name, last_name)
-);
